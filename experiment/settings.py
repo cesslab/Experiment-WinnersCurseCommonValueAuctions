2ba72d7a8dc4d09062f7509286d879816de91669
cesslab <anwarruff@gmail.com>
2018-03-27 11:15:38 -0400

    added session config params

--- experiment/settings.py
+++ experiment/settings.py
@@ -16,12 +16,12 @@ SESSION_CONFIG_DEFAULTS = {
 }
 
 SESSION_CONFIGS = [
-    #{
-    #    'name': 'public_goods',
-    #    'display_name': "Public Goods",
-    #    'num_demo_participants': 3,
-    #    'app_sequence': ['public_goods', 'payment_info'],
-    #},
+    {
+        'name': 'auction',
+        'display_name': "Auction Experiment",
+        'num_demo_participants': 2,
+        'app_sequence': ['auction'],
+    },
 ]
 
 
