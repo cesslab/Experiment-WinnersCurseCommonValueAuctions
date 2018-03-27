9a4fe3622ee7eef5213162c82dd2f1867b620445
cesslab <anwarruff@gmail.com>
2018-03-27 11:13:26 -0400

    moved auction to experiment_models

--- experiment/auction/models.py
+++ experiment/auction/models.py
@@ -3,6 +3,8 @@ from otree.api import (
     Currency as c, currency_range
 )
 
+from .experiment_models import Treatment
+
 
 author = 'Anwar A Ruff'
 
@@ -11,28 +13,11 @@ https://github.com/cesslab/Experiment-WinnersCurseCommonValueAuctions
 """
 
 
-class Auction:
-    def __init__(self, val_low, val_high, prob_low, prob_high):
-        self.val_low = val_low
-        self.val_high = val_high
-        self.prob_low = prob_low
-        self.prob_high = prob_high
-
-
 class Constants(BaseConstants):
     name_in_url = 'wc'
     players_per_group = 2
-    num_rounds = 1
-    auctions = [
-        [
-            Auction(val_low=[0.2], val_high=[0.2], prob_low=[0.4], prob_high=[0.7]),
-            Auction(val_low=[0.1], val_high=[0.3], prob_low=[0.7], prob_high=[0.1])
-        ],
-        [
-            Auction(val_low=[0.2], val_high=[0.2], prob_low=[0.4], prob_high=[0.7]),
-            Auction(val_low=[0.1], val_high=[0.3], prob_low=[0.7], prob_high=[0.1])
-        ],
-    ]
+    num_rounds = 2
+    auctions = Treatment.t1_auction_collection()
 
 
 class Subsession(BaseSubsession):
@@ -44,4 +29,4 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+    preference = models.IntegerField(choices=[1, 2, 3])
