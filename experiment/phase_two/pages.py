ff68f83dee395db4a353acd960c1349eae33098d
cesslab <anwarruff@gmail.com>
2018-03-30 12:36:11 -0400

    added allocation page for phase 2

--- experiment/phase_two/pages.py
+++ experiment/phase_two/pages.py
@@ -7,6 +7,15 @@ class InstructionsPage(Page):
     pass
 
 
+class AllocationPage(Page):
+    form_model = 'player'
+    form_fields = ['preference']
+
+    def vars_for_template(self):
+        auction = Constants.auctions.left_auction(self.round_number)
+        return {'auction': auction}
+
+
 class ResultsWaitPage(WaitPage):
 
     def after_all_players_arrive(self):
