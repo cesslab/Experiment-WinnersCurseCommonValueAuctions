cb4746200569bcc1e832d5d1bd095e4eeaefee35
aaruff <anwarruff@gmail.com>
2018-04-09 14:37:15 -0400

    added min max validation, in addition to click event checking

--- experiment/phase_two/pages.py
+++ experiment/phase_two/pages.py
@@ -10,21 +10,27 @@ class InstructionsPage(Page):
 
 class CutoffSelectionPage(Page):
     form_model = 'player'
-    form_fields = ['cutoff']
+    form_fields = ['cutoff', 'clicked']
 
     def vars_for_template(self):
         auction = Constants.auctions.auction(self.round_number)
-        return {'auction': auction}
+        min_max = Constants.auctions.auction_min_max(self.round_number)
+        return {
+            'auction': auction,
+            'auction_min': min_max[0],
+            'auction_max': min_max[1]}
 
+    def cutoff_max(self):
+        min_max = Constants.auctions.auction_min_max(self.round_number)
+        return min_max[1]
 
-class ResultsWaitPage(WaitPage):
+    def cutoff_min(self):
+        min_max = Constants.auctions.auction_min_max(self.round_number)
+        return min_max[0]
 
-    def after_all_players_arrive(self):
-        pass
-
-
-class Results(Page):
-    pass
+    def error_message(self, values):
+        if not int(values['clicked']) == 1:
+            return 'You must make a bid for this auction.'
 
 
 page_sequence = [
