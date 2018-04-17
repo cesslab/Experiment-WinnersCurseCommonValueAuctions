1fde5a5a7920831929fdf01e6ff6f39b3e146271
cesslab <anwarruff@gmail.com>
2018-04-17 11:26:34 -0400

    added test for auction min max

--- experiment/test/test_phase_two_auctions.py
+++ experiment/test/test_phase_two_auctions.py
@@ -1,5 +1,5 @@
 from auction.factory import AuctionCollectionFactory
-from auction.auctions import PhaseOneAuctionCollection, Auction
+from auction.auctions import PhaseTwoAuctionCollection, Auction
 
 
 def test_auction_collection_not_empty():
@@ -7,9 +7,14 @@ def test_auction_collection_not_empty():
 
 
 def test_auction_collection_contain_auctions():
-    auction_collection = AuctionCollectionFactory.phase_one_auctions()
-    assert type(auction_collection) is PhaseOneAuctionCollection
+    auction_collection = AuctionCollectionFactory.phase_two_auctions()
+    assert type(auction_collection) is PhaseTwoAuctionCollection
 
 
 def test_rounds_not_zero():
     assert AuctionCollectionFactory.phase_two_rounds() > 0
+
+
+def test_auction_min_is_less_than_max():
+    for auction in AuctionCollectionFactory.phase_two_auctions():
+        assert auction.min_value <= auction.max_value
