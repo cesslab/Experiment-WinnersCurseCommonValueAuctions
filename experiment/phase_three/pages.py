7902290289fa8b11e9da17ca28d89897a9b31719
aaruff <anwarruff@gmail.com>
2018-04-17 15:30:02 -0400

    added bid form validation

--- experiment/phase_three/pages.py
+++ experiment/phase_three/pages.py
@@ -1,4 +1,3 @@
-from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
@@ -19,14 +18,23 @@ class BidPage(Page):
     form_fields = ['bid']
 
     def vars_for_template(self):
-        auction = Constants.auctions.auction(self.round_number)
-        signal = Constants.auctions.signal(self.round_number)
+        auction_collection = self.player.participant.vars['phase_three_auctions']
+        auction = auction_collection.auction(self.round_number)
+        signal = auction_collection.signal(self.round_number)
 
         return {
             'auction': auction,
             'signal': signal,
             'low_update': auction.low_update(signal),
             'high_update': auction.high_update(signal)}
+
+    def bid_min(self):
+        return 0
+
+    def bid_max(self):
+        auction_collection = self.player.participant.vars['phase_three_auctions']
+        auction = auction_collection.auction(self.round_number)
+        return auction.max_value
 
 
 class Results(Page):
