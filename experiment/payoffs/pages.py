77625663eff516ce58e36415972dee073976880e
aaruff <anwarruff@gmail.com>
2018-04-19 16:39:49 -0400

    commented out payoff calculations

--- experiment/payoffs/pages.py
+++ experiment/payoffs/pages.py
@@ -1,6 +1,4 @@
-from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
-from .models import Constants
 
 
 class MyPage(Page):
@@ -10,6 +8,32 @@ class MyPage(Page):
 class ResultsWaitPage(WaitPage):
 
     def after_all_players_arrive(self):
+        # Instead of performing all of the querying here it is better to spread out acquisition of the
+        # payoff auctions, bids, and cutoffs  in their respective rounds
+        #
+        # ----------------------------------------------------------------------------------------------
+        # players = self.group.get_players()[:]
+        # for i in range(len(players)):
+        #     for player in players:
+        #         # find a pair member
+        #         pair_member = random.choice(players[:i] + players[i + 1:])
+        #         # Choose a random round for phase one
+        #         num_phase_one_rounds = PhaseOneConstants.num_rounds
+        #         round_a = random.randint(1, num_phase_one_rounds)
+        #         round_b = random.randint(1, num_phase_one_rounds)
+        #         # retrieve the player's selected preference for round a ad b
+        #         phase_one_auction_collection = player.participant.vars['phase_one_auctions']
+        #
+        #         preference_round_a = player.in_round(round_a).preference
+        #         if preference_round_a == PhaseOneConstants.INDIFFERENT:
+        #             if random.random() > 0.5:
+        #                 auction = phase_one_auction_collection.left_auction(round_a)
+        #             else:
+        #                 auction = phase_one_auction_collection.right_auction(round_a)
+        #         else:
+        #             auction = phase_one_auction_collection.auctions[preference_round_a]
+
+        # preference_round_b = player.in_round(round_b).preference
         pass
 
 
@@ -18,7 +42,6 @@ class Results(Page):
 
 
 page_sequence = [
-    MyPage,
     ResultsWaitPage,
     Results
 ]
