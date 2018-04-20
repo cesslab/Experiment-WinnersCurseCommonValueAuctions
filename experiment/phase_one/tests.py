bf1fe3a695f013efc3fe0a0fad5c755d2f4697e1
cesslab <anwarruff@gmail.com>
2018-04-20 18:16:43 -0400

    added tests for round_a and round_b vars

--- experiment/phase_one/tests.py
+++ experiment/phase_one/tests.py
@@ -1,10 +1,13 @@
 from otree.api import Bot, SubmissionMustFail
 from phase_one import pages
 
+from auction.auctions import Auction
+
 
 class PlayerBot(Bot):
 
     def play_round(self):
+        participant_vars = self.player.participant.vars
         if self.round_number == 1:
             yield (pages.InstructionsPage)
 
@@ -18,16 +21,22 @@ class PlayerBot(Bot):
             assert self.player.preference == 0
         elif self.round_number == 2:
             # SelectAuctionPage: test right preferred selection
-            auction_collection = self.player.participant.vars['phase_one_auctions']
+            auction_collection = participant_vars['phase_one_auction_collection']
             right_auction = auction_collection.right_auction(self.round_number)
             yield (pages.SelectAuctionPage, {'preference': right_auction.aid})
-            assert self.player.preference == right_auction.aid, "actual preference was {}".format(self.player.preference)
+            assert self.player.preference == right_auction.aid, "actual preference was {}".format(
+                self.player.preference)
         else:
             # SelectAuctionPage: test left preferred selection
-            auction_collection = self.player.participant.vars['phase_one_auctions']
+            auction_collection = participant_vars['phase_one_auction_collection']
             left_auction = auction_collection.left_auction(self.round_number)
             yield (pages.SelectAuctionPage, {'preference': left_auction.aid})
             assert self.player.preference == left_auction.aid, "actual preference was {}".format(self.player.preference)
 
+        payoff_round_a = participant_vars['round_a']
+        if self.round_number == payoff_round_a:
+            assert 'auction_a_id' in participant_vars
 
-
+        payoff_round_b = participant_vars['round_b']
+        if self.round_number == payoff_round_b:
+            assert 'auction_b_id' in participant_vars
