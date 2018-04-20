f9740b9bf34f1f68bf138353ef3cf53b6e6c20eb
cesslab <anwarruff@gmail.com>
2018-04-20 18:17:28 -0400

    added before_next_page to store the player's bid

--- experiment/phase_three/pages.py
+++ experiment/phase_three/pages.py
@@ -1,4 +1,4 @@
-from ._builtin import Page, WaitPage
+from ._builtin import Page
 from .models import Constants
 
 
@@ -7,18 +7,12 @@ class InstructionsPage(Page):
         return self.round_number == Constants.INSTRUCTIONS_ROUND
 
 
-class ResultsWaitPage(WaitPage):
-
-    def after_all_players_arrive(self):
-        pass
-
-
 class BidPage(Page):
     form_model = 'player'
     form_fields = ['bid']
 
     def vars_for_template(self):
-        auction_collection = self.player.participant.vars['phase_three_auctions']
+        auction_collection = self.player.participant.vars['phase_three_auction_collection']
         auction = auction_collection.auction(self.round_number)
         signal = auction_collection.signal(self.round_number)
 
@@ -29,15 +23,24 @@ class BidPage(Page):
             'high_update': auction.high_update(signal)}
 
     def bid_error_message(self, bid):
-        auction_collection = self.player.participant.vars['phase_three_auctions']
+        auction_collection = self.player.participant.vars['phase_three_auction_collection']
         auction = auction_collection.auction(self.round_number)
         max_bid = auction.max_value
         if not 0 <= bid <= max_bid:
             return 'The bid value must be between 0 and {}.'.format(max_bid)
 
+    def before_next_page(self):
+        """
+        If the auction displayed for this round is payoff relevant, the bid is saved to the participants var
+        list.
+        :return: None
+        """
+        participant_vars = self.player.participant.vars
+        auction_collection = participant_vars['phase_three_auction_collection']
+        auction = auction_collection.auction(self.round_number)
+        signal = auction_collection.signal(self.round_number)
 
-class Results(Page):
-    pass
+        participant_vars['auctions'][auction.aid].bids[signal] = self.player.bid
 
 
 page_sequence = [
