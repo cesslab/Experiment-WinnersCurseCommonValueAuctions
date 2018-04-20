cb775dbb1dd4ecf7bbed0f0f8e107cb35017fce4
cesslab <anwarruff@gmail.com>
2018-04-20 18:18:07 -0400

    added before_next_page for storing the players cutoff

--- experiment/phase_two/pages.py
+++ experiment/phase_two/pages.py
@@ -12,12 +12,12 @@ class CutoffSelectionPage(Page):
     form_fields = ['cutoff', 'clicked']
 
     def vars_for_template(self):
-        auction_collection = self.player.participant.vars['phase_two_auctions']
+        auction_collection = self.player.participant.vars['phase_two_auction_collection']
         auction = auction_collection.auction(self.round_number)
         return {'auction': auction}
 
     def cutoff_max(self):
-        auction_collection = self.player.participant.vars['phase_two_auctions']
+        auction_collection = self.player.participant.vars['phase_two_auction_collection']
         auction = auction_collection.auction(self.round_number)
         return auction.max_value
 
@@ -27,6 +27,17 @@ class CutoffSelectionPage(Page):
     def error_message(self, values):
         if not int(values['clicked']) == 1:
             return 'You must make a bid for this auction.'
+
+    def before_next_page(self):
+        """
+        If the auction displayed for this round is payoff relevant, the cutoff is saved to the participants var
+        list.
+        :return: None
+        """
+        participant_vars = self.player.participant.vars
+        auction_collection = self.player.participant.vars['phase_two_auction_collection']
+        auction = auction_collection.auction(self.round_number)
+        participant_vars['auctions'][auction.aid].cutoff = self.player.cutoff
 
 
 page_sequence = [
