8c919e6775415037c1f9d1b37aab63cc1921986b
cesslab <anwarruff@gmail.com>
2018-04-25 08:27:51 -0400

    implemented subject payoffs

--- payoffs/pages.py
+++ payoffs/pages.py
@@ -2,6 +2,8 @@ import random
 
 from ._builtin import Page, WaitPage
 
+from .models import Constants
+
 
 class MyPage(Page):
     pass
@@ -15,29 +17,65 @@ class ResultsWaitPage(WaitPage):
         #
         # ----------------------------------------------------------------------------------------------
         players = self.group.get_players()[:]
+        payoff_bid = []
         for i in range(len(players)):
             for player in players:
                 other_player = random.choice(players[:i] + players[i + 1:])
-                if self.player_win_auction(player, other_player):
-                    pass
-                else:
-                    player.payoff = 0
+                payoff_bid.append(self.bid_payoff(player, other_player))
 
-    def player_win_auction(self, player, other):
+        cutoff_bid = []
+        for i in range(len(players)):
+            for player in players:
+                other_player = random.choice(players[:i] + players[i + 1:])
+                cutoff_bid.append(self.cutoff_payoff(player, other_player))
+
+        for i in range(len(players)):
+            for player in players:
+                player.payoff = (float(cutoff_bid[i]) + float(payoff_bid[i])) / 2
+
+    def cutoff_payoff(self, player, other):
+        player_participant_vars = player.participant.vars
+        bid = player_participant_vars['auction_b_id']
+        player_auction = player_participant_vars['auctions'][bid]
+        cutoff = player_auction.cutoff
+        max_value = player_auction.max_value
+        min_value = player_auction.min_value
+        r = (max_value - min_value) * random.random() + min_value
+        if r >= cutoff:
+            return Constants.Endowment - cutoff
+        else:
+            return self.bid_payoff(player, other)
+
+    def bid_payoff(self, player, other):
+        # Determine if the player wins the auction
         player_participant_vars = player.participant.vars
         other_participant_vars = other.participant.vars
         aid = player_participant_vars['auction_a_id']
         player_auction = player_participant_vars['auctions'][aid]
         other_auction = other_participant_vars['auctions'][aid]
 
-        # Get the player's bid for the first randomly chosen payoff relevant auction-signal pair
+        player_random_signal = player_auction.random_signal
+        other_random_signal = random.choice(player_auction.signals)
+
         player_bid = player_auction.bids[player_auction.random_signal]
-        other_bid = other_auction.bids[player_auction.random_signal]
+        other_bid = other_auction.bids[random.choice(player_auction.signals)]
 
         if player_bid == other_bid:
-            return random.randint(0, 1) == 1
+            player_wins = random.randint(0, 1) == 1
         else:
-            return player_bid > other_bid
+            player_wins = player_bid > other_bid
+
+        if not player_wins:
+            return Constants.Endowment
+        else:
+            low_prob = player_auction.low_probability(player_random_signal, other_random_signal)
+            r = random.random()
+            if r < low_prob:
+                low_value = player_auction.low_value(player_random_signal, other_random_signal)
+                return Constants.Endowment - player_bid + low_value
+            else:
+                high_value = player_auction.high_value(player_random_signal, other_random_signal)
+                return Constants.Endowment - player_bid + high_value
 
 
 class Results(Page):
