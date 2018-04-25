67d1a92cd0b24be49bed32755fb2942351643327
cesslab <anwarruff@gmail.com>
2018-04-25 08:30:15 -0400

    fix: incorrect method of iterating over dictionary

--- test/test_auction.py
+++ test/test_auction.py
@@ -4,5 +4,5 @@ from auction.factory import AuctionCollectionFactory
 def test_auction_collection_contain_auctions():
     auctions = AuctionCollectionFactory.auctions()
     for aid, auction in auctions.items():
-        for signal, bid in auction.bids:
+        for signal, bid in auction.bids.items():
             assert signal in auction.signals
