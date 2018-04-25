161d40f9c093ce0b07f506edd96e342056854b82
aaruff <anwarruff@gmail.com>
2018-04-25 12:23:15 -0400

    added payoff phase test

--- payoffs/tests.py
+++ payoffs/tests.py
@@ -1,9 +1,8 @@
-from otree.api import Currency as c, currency_range
-from . import pages
-from ._builtin import Bot
-from .models import Constants
+from otree.api import Bot
+from payoffs import pages, models
 
 
 class PlayerBot(Bot):
     def play_round(self):
-        pass
+        yield (pages.Results)
+        assert self.player.payoff is not None
