8f231f7081700d94ff9ef4cf6862a0f90f82cb25
aaruff <anwarruff@gmail.com>
2018-05-07 17:10:03 -0400

    renamed phase payment functions

--- payoffs/pages.py
+++ payoffs/pages.py
@@ -21,19 +21,19 @@ class ResultsWaitPage(WaitPage):
         for i in range(len(players)):
             for player in players:
                 other_player = random.choice(players[:i] + players[i + 1:])
-                payoff_bid.append(self.bid_payoff(player, other_player))
+                payoff_bid.append(self.phase_1_3_payoff(player, other_player))
 
         cutoff_bid = []
         for i in range(len(players)):
             for player in players:
                 other_player = random.choice(players[:i] + players[i + 1:])
-                cutoff_bid.append(self.cutoff_payoff(player, other_player))
+                cutoff_bid.append(self.phase_2_3_payoff(player, other_player))
 
         for i in range(len(players)):
             for player in players:
                 player.payoff = (float(cutoff_bid[i]) + float(payoff_bid[i])) / 2
 
-    def cutoff_payoff(self, player, other):
+    def phase_2_3_payoff(self, player, other):
         player_participant_vars = player.participant.vars
         bid = player_participant_vars['auction_b_id']
         player_auction = player_participant_vars['auctions'][bid]
@@ -44,38 +44,55 @@ class ResultsWaitPage(WaitPage):
         if r >= cutoff:
             return Constants.Endowment - cutoff
         else:
-            return self.bid_payoff(player, other)
+            return self.phase_1_3_payoff(player, other)
 
-    def bid_payoff(self, player, other):
+    def phase_1_3_payoff(self, player, other):
         # Determine if the player wins the auction
         player_participant_vars = player.participant.vars
         other_participant_vars = other.participant.vars
+
+        player.round_1 = player_participant_vars['round_a']
+
         aid = player_participant_vars['auction_a_id']
+        player.auction_1 = aid
+
+        choice = player_participant_vars['auction_a_id_choice']
+        player.choice_1 = choice
+
         player_auction = player_participant_vars['auctions'][aid]
         other_auction = other_participant_vars['auctions'][aid]
 
         player_random_signal = player_auction.random_signal
+        player.signal_1 = player_random_signal
         other_random_signal = random.choice(player_auction.signals)
+        player.other_signal_1 = player_random_signal
 
         player_bid = player_auction.bids[player_auction.random_signal]
+        player.bid_1 = player_bid
         other_bid = other_auction.bids[random.choice(player_auction.signals)]
+        player.other_bid_1 = other_bid
 
         if player_bid == other_bid:
             player_wins = random.randint(0, 1) == 1
         else:
             player_wins = player_bid > other_bid
 
+        player.winner_1 = player_wins
+
         if not player_wins:
             return Constants.Endowment
         else:
             low_prob = player_auction.low_probability(player_random_signal, other_random_signal)
             r = random.random()
+            player.random_low_prob_1 = r
             if r < low_prob:
                 low_value = player_auction.low_value(player_random_signal, other_random_signal)
-                return Constants.Endowment - player_bid + low_value
+                player.earnings_1 = player_bid + low_value
+                return player_bid + low_value
             else:
                 high_value = player_auction.high_value(player_random_signal, other_random_signal)
-                return Constants.Endowment - player_bid + high_value
+                player.earnings_1 = player_bid + high_value
+                return player_bid + high_value
 
 
 class Results(Page):
@@ -84,5 +101,5 @@ class Results(Page):
 
 page_sequence = [
     ResultsWaitPage,
-    Results
+    Results,
 ]
