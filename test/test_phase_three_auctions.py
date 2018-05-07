95646140ec87f78b9dc4e96c41b67d4a542e704f
aaruff <anwarruff@gmail.com>
2018-05-07 13:57:44 -0400

    added test for signal value range

--- test/test_phase_three_auctions.py
+++ test/test_phase_three_auctions.py
@@ -26,3 +26,12 @@ def test_factory_auction_types():
     rounds = AuctionCollectionFactory.phase_three_rounds()
     for game_round in range(1, rounds + 1):
         assert (type(auction_collection.auction(game_round)) is Auction)
+
+
+def test_auction_signal_within_range():
+    auction_signal_pair = AuctionCollectionFactory.phase_three_auction_collection().auction_signal_pairs
+    for pair in auction_signal_pair:
+        auction = pair['auction']
+        signal = pair['signal']
+        assert auction.range_low() <= signal <= auction.range_high(), "auction {}".format(auction.aid)
+
