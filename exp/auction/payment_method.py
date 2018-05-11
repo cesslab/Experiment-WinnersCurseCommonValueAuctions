acb87dd8995b1d865f5016032dd18b6dbca2cfa8
aaruff <anwarruff@gmail.com>
2018-05-11 17:17:02 -0400

    Added MethodOneResults

--- exp/auction/payment_method.py
+++ exp/auction/payment_method.py
@@ -1,43 +1,50 @@
 import random
 
-from exp.auction import Auction
+from exp.auctions import Auction
+from exp.phases import PhaseOne, PhaseThree
+from exp.experiment import Experiment
 
 
-class PaymentMethodOne:
-    INDIFFERENT = 0
+class PaymentMethod:
+    def __init__(self, player_experiment: Experiment, other_experiment: Experiment):
+        self.player_experiment = player_experiment
+        self.other_experiment = other_experiment
 
-    def __init__(self, phase_one_round: int = 0):
-        self.phase_one_round = phase_one_round
-        self.selected_auction = None
-        self.selected_signal = None
+    def method_one_payment(self):
+        results = MethodOneResults()
+        results.round_number = self.player_experiment.phase_one.random_round()
+
+        results.left_auction = self.player_experiment.phase_one.left_auction(results.round_number)
+        results.right_auction = self.player_experiment.phase_one.right_auction(results.round_number)
+
+        results.preferred_position = self.player_experiment.phase_one.preferred_position(results.round_number)
+        if results.preferred_position == self.player_experiment.phase_one.INDIFFERENT:
+            results.random_val = random.random()
+            if results.random_val < 0.5:
+                results.auction = self.player_experiment.phase_one.left_auction(results.round_number)
+            else:
+                results.auction = self.player_experiment.phase_one.right_auction(results.round_number)
+        else:
+            results.auction = self.player_experiment.phase_one.preffered_auction(results.round_number)
+
+        results.random_signal = results.auction.random_signal()
+        results.bid = results.auction.bids[results.random_signal]
+
+
+
+
+
+
+
+
+class MethodOneResults:
+    def __init__(self):
+        self.round_number = - 1
+        self.preferred_position = - 1
+        self.random_value = -1
+        self.auction = None
         self.left_auction = None
         self.right_auction = None
-        self.phase_one_random = -1
-
-    def get_selected_side(self) -> str:
-        return 'Left' if self.left_auction.aid == self.selected_auction.aid else 'Right'
-
-    def is_phase_one_payment_round(self, current_round: int) -> bool:
-        return current_round == self.phase_one_round
-
-    def set_phase_one_auctions(self, left_auction: Auction, right_auction: Auction, selected_auction_id: int):
-        self.left_auction = left_auction
-        self.right_auction = right_auction
-        if selected_auction_id == PaymentMethodOne.INDIFFERENT:
-            self.phase_one_random = random.random()
-            if self.phase_one_random < 0.5:
-                self.selected_auction = left_auction
-            else:
-                self.selected_auction = right_auction
-        else:
-            if selected_auction_id == left_auction.aid:
-                self.selected_auction = left_auction
-            else:
-                self.selected_auction = right_auction
-
-
-class PaymentMethodTwo(PaymentMethodOne):
-    def __init__(self, phase_one_round: int = 0, phase_two_round: int = 0):
-        super().__init__(phase_one_round)
-        self.phase_two_round = phase_two_round
-
+        self.bid = -1
+        self.other_bid = -1
+        self.random_signal = -1
