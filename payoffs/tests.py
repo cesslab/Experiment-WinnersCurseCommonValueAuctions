3e4a2f79bcefe073096efb528ee66f7e7fc9c542
aaruff <anwarruff@gmail.com>
2018-05-14 16:57:40 -0400

    updated to reflect refactoring changes

--- payoffs/tests.py
+++ payoffs/tests.py
@@ -4,5 +4,5 @@ from payoffs import pages, models
 
 class PlayerBot(Bot):
     def play_round(self):
-        yield (pages.Results)
+        yield (pages.PaymentOneResults)
         assert self.player.payoff is not None
