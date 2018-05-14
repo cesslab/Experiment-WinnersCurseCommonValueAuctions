a655f48a27f835e25e7e7957fd849c6298749185
aaruff <anwarruff@gmail.com>
2018-05-14 17:00:06 -0400

    Added method 1 payment data

--- payoffs/pages.py
+++ payoffs/pages.py
@@ -1,11 +1,9 @@
 import random
 
-from exp.auction.factory import AuctionFactory
-
 from ._builtin import Page, WaitPage
 
 from exp.util import Participant
-from exp.auction.payment_method import PaymentMethod
+from exp.payment import PaymentMethod
 
 
 class MyPage(Page):
@@ -18,24 +16,52 @@ class ResultsWaitPage(WaitPage):
         players = self.group.get_players()[:]
         for i in range(len(players)):
             for player in players:
+                player_id = player.participant.id_in_session
                 other_player = random.choice(players[:i] + players[i + 1:])
+                other_id = other_player.participant.id_in_session
                 experiment = Participant.get_experiment(player)
                 other_experiment = Participant.get_experiment(other_player)
-                payment_method = PaymentMethod(experiment, other_experiment)
+                payment_method = PaymentMethod(player_id, other_id, experiment, other_experiment)
 
-                method_one_results = payment_method.method_one_payment()
-                method_two_results = payment_method.method_two_payment()
+                Participant.set_payment_one_results(player, payment_method.method_one_payment())
+                Participant.set_payment_two_results(player, payment_method.method_two_payment())
 
 
-class Results(Page):
+class PaymentOneResults(Page):
     def vars_for_template(self):
-        auctions = AuctionFactory.auctions()
-        aid = self.player.participant.vars['auction_a_id']
+        experiment = Participant.get_experiment(self.player)
+        results = Participant.get_payment_one_results(self.player)
 
-        return {'auction_1': auctions[aid]}
+        random_position = ''
+        if results.preferred_position == experiment.phase_one.LEFT:
+            preferred_position = 'Left'
+        elif results.preferred_position == experiment.phase_one.RIGHT:
+            preferred_position = 'Right'
+        else:
+            preferred_position = 'Indifferent'
+            random_position = 'Left' if results.left_auction.aid == results.auction.aid else 'Right'
+
+        return {
+            'preferred_position': preferred_position,
+            'left_auction': results.left_auction,
+            'right_auction': results.right_auction,
+            'auction': results.auction,
+            'random_position': random_position,
+            'bid': results.bid,
+            'others_bid': results.other_bid,
+            'winner': results.win_lottery,
+            'signal': results.random_signal,
+            'others_signal': results.other_random_signal,
+            'low_value': results.low_value,
+            'high_value': results.high_value,
+            'low_prob': results.low_prob * 100,
+            'high_prob': results.high_prob * 100,
+            'high_chosen': results.high_prize_chosen,
+            'earnings': results.earnings
+        }
 
 
 page_sequence = [
     ResultsWaitPage,
-    Results,
+    PaymentOneResults,
 ]
