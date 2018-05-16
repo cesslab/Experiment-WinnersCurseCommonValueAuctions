538c8f1d4fffe5faff58cc4a4e1e45f7b2120b1d
aaruff <anwarruff@gmail.com>
2018-05-16 16:45:58 -0400

    Added method 2 template vars

--- payoffs/pages.py
+++ payoffs/pages.py
@@ -3,7 +3,7 @@ import random
 from ._builtin import Page, WaitPage
 
 from exp.util import Participant
-from exp.payment import PaymentMethod
+from exp.payment import PaymentMethod, Results
 
 
 class MyPage(Page):
@@ -23,11 +23,11 @@ class ResultsWaitPage(WaitPage):
                 other_experiment = Participant.get_experiment(other_player)
                 payment_method = PaymentMethod(player_id, other_id, experiment, other_experiment)
 
-                Participant.set_payment_one_results(player, payment_method.method_one_payment())
-                Participant.set_payment_two_results(player, payment_method.method_two_payment())
+                Participant.set_payment_one_results(player, payment_method.method_one_payment(Results()))
+                Participant.set_payment_two_results(player, payment_method.method_two_payment(Results()))
 
 
-class PaymentOneResults(Page):
+class MethodOneResults(Page):
     def vars_for_template(self):
         experiment = Participant.get_experiment(self.player)
         results = Participant.get_payment_one_results(self.player)
@@ -57,11 +57,63 @@ class PaymentOneResults(Page):
             'low_prob': results.low_prob * 100,
             'high_prob': results.high_prob * 100,
             'high_chosen': results.high_prize_chosen,
-            'earnings': results.earnings
+            'earnings': results.earnings,
+            'realized': results.realized,
+            'auction_type': results.auction.atype,
+            'low_prize_chosen': results.low_prize_chosen,
+            'high_prize_chosen': results.high_prize_chosen,
         }
+
+
+class MethodTwoResults(Page):
+    def vars_for_template(self):
+        experiment = Participant.get_experiment(self.player)
+        results = Participant.get_payment_two_results(self.player)
+
+        context = {
+            'cutoff_auction': results.cutoff_auction,
+            'cutoff': results.cutoff,
+            'random_offer': results.random_offer,
+            'offer_accepted': results.offer_accepted,
+        }
+
+        if not results.offer_accepted:
+            random_position = 'Left' if results.left_auction.aid == results.auction.aid else 'Right'
+
+            if results.preferred_position == experiment.phase_one.LEFT:
+                preferred_position = 'Left'
+            elif results.preferred_position == experiment.phase_one.RIGHT:
+                preferred_position = 'Right'
+            else:
+                preferred_position = 'Indifferent'
+            context.update({
+                'preferred_position': preferred_position,
+                'left_auction': results.left_auction,
+                'right_auction': results.right_auction,
+                'auction': results.auction,
+                'random_position': random_position,
+                'bid': results.bid,
+                'others_bid': results.other_bid,
+                'winner': results.win_lottery,
+                'signal': results.random_signal,
+                'others_signal': results.other_random_signal,
+                'low_value': results.low_value,
+                'high_value': results.high_value,
+                'low_prob': results.low_prob * 100,
+                'high_prob': results.high_prob * 100,
+                'high_chosen': results.high_prize_chosen,
+                'earnings': results.earnings,
+                'realized': results.realized,
+                'auction_type': results.auction.atype,
+                'low_prize_chosen': results.low_prize_chosen,
+                'high_prize_chosen': results.high_prize_chosen,
+            })
+
+        return context
 
 
 page_sequence = [
     ResultsWaitPage,
-    PaymentOneResults,
+    MethodOneResults,
+    MethodTwoResults,
 ]
