8e8b9da127dc793f644315a6679cc7218da024f3
aaruff <anwarruff@gmail.com>
2018-05-24 12:29:34 -0400

    added min/max cutoff vars

--- exp/lottery.py
+++ exp/lottery.py
@@ -5,13 +5,17 @@ class Lottery:
     RED = 0
     BLUE = 1
     VALUE = 0
-    PROBABILITY = 1
+    NUMBER = 1
 
-    def __init__(self, lid: int, ltype: int, matrix: List = []):
+    def __init__(self, lid: int, ltype: int, matrix: List, min_cutoff: int, max_cutoff: int):
         self.lid = lid
         self.ltype = ltype
         self.matrix = matrix
+        self.max_cutoff = max_cutoff
+        self.min_cutoff = min_cutoff
+
         self.cutoff = None
+        self.bet = None
 
     @property
     def red_values(self):
@@ -22,12 +26,12 @@ class Lottery:
         return self.matrix[Lottery.VALUE][Lottery.BLUE]
 
     @property
-    def red_probabilities(self):
-        return self.matrix[Lottery.PROBABILITY][Lottery.RED]
+    def red_number(self):
+        return self.matrix[Lottery.NUMBER][Lottery.RED]
 
     @property
-    def blue_probabilities(self):
-        return self.matrix[Lottery.PROBABILITY][Lottery.BLUE]
+    def blue_number(self):
+        return self.matrix[Lottery.NUMBER][Lottery.BLUE]
 
     def __str__(self):
         return 'Lottery ' % self.lid
@@ -46,5 +50,7 @@ class LotteryFactory:
                 lid=params['id'],
                 ltype=params['type'],
                 matrix=params['matrix'],
+                min_cutoff=params['min_cutoff'],
+                max_cutoff=params['max_cutoff'],
             )
         return lotteries
