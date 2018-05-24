0365fa96008552c46aae0f371021ac9b68f47343
aaruff <anwarruff@gmail.com>
2018-05-24 15:38:15 -0400

    added total, min, and max to lottery

--- exp/lottery.py
+++ exp/lottery.py
@@ -7,9 +7,10 @@ class Lottery:
     VALUE = 0
     NUMBER = 1
 
-    def __init__(self, lid: int, ltype: int, matrix: List, min_cutoff: int, max_cutoff: int):
+    def __init__(self, lid: int, ltype: int, total: int, matrix: List, min_cutoff: int, max_cutoff: int):
         self.lid = lid
         self.ltype = ltype
+        self.total = total
         self.matrix = matrix
         self.max_cutoff = max_cutoff
         self.min_cutoff = min_cutoff
@@ -18,19 +19,21 @@ class Lottery:
         self.bet = None
 
     @property
-    def red_values(self):
+    def red_value(self):
         return self.matrix[Lottery.VALUE][Lottery.RED]
 
     @property
-    def blue_values(self):
+    def blue_value(self):
         return self.matrix[Lottery.VALUE][Lottery.BLUE]
 
     @property
     def red_number(self):
+        assert self.ltype == 1
         return self.matrix[Lottery.NUMBER][Lottery.RED]
 
     @property
     def blue_number(self):
+        assert self.ltype == 1
         return self.matrix[Lottery.NUMBER][Lottery.BLUE]
 
     def __str__(self):
@@ -49,8 +52,9 @@ class LotteryFactory:
             lotteries[lid] = Lottery(
                 lid=params['id'],
                 ltype=params['type'],
+                total=params['total'],
                 matrix=params['matrix'],
-                min_cutoff=params['min_cutoff'],
-                max_cutoff=params['max_cutoff'],
+                min_cutoff=params['min'],
+                max_cutoff=params['max'],
             )
         return lotteries
