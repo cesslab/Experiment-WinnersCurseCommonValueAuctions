19cd72044117d852250494a60d4ecc46030758eb
cesslab <anwarruff@gmail.com>
2018-05-31 10:46:09 -0400

    allowed passcode to be empty in rounds where it is not used

--- phase_four/models.py
+++ phase_four/models.py
@@ -31,4 +31,4 @@ class Player(BasePlayer):
     clicked = models.IntegerField()
     lottery = models.IntegerField(blank=False)
     die_side = models.IntegerField()
-    pass_code = models.IntegerField()
+    pass_code = models.IntegerField(blank=True)
